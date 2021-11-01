93e8e91e9685895c12bf21feba482fdb103f0d1b
demian <demian@kcd.co.kr>
2021-11-01 13:43:47 +0900

    Add react-query QueryClientProvider and useQueryClient

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [client] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={client}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const queryClient = useQueryClient();
   return (
     <div className={styles.container}>
       <Head>
